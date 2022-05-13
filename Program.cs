776c3b080920f9e59c99bfe48eb7c8f7e3417360
Gabriel <gabrielrodrigo380@hotmail.com>
2022-05-13 19:07:36 -0300

    Projeto Tabuada

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        int tabuada, contador = 0, resultado;
+
+        Console.WriteLine("Qual a tabuada?");
+        tabuada = Convert.ToInt32(Console.ReadLine());
+
+
+        while(contador <= 10)
+        {
+            resultado = tabuada * contador;
+            Console.WriteLine("{0} x {1} = {2}", tabuada, contador, resultado);
+            contador++;
+        }
+    }
+}
